ad49c45fb5e804691df5ac26498730a6f924d69e
victoracho <32299455+victoracho@users.noreply.github.com>
2022-09-18 17:57:35 -0400

    adding seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,41 +27,48 @@ class DatabaseSeeder extends Seeder
         $plan = new Plan;
         $plan->name = 'Grupal';
         $plan->save;
-        
+
         $plan = new Plan;
         $plan->name = 'Banca Asistencia';
         $plan->save;
 
         $assistance = new Assistance;
         $assistance->name = 'Asistencia funeraria nacional e internacional';
+        $assistance->description = 'Asistencia funeraria nacional e internacional';
         $assistance->save();
 
         $assistance = new Assistance;
         $assistance->name = 'Asistencia financiera en duelo';
+        $assistance->description = 'Asistencia financiera en duelo';
         $assistance->save();
 
         $assistance = new Assistance;
         $assistance->name = 'Asistencia médica telefónica y domiciliaria';
+        $assistance->description = 'Asistencia médica telefónica y domiciliaria';
         $assistance->save();
 
 
         $assistance = new Assistance;
         $assistance->name = 'Combo citas médicas';
+        $assistance->description = 'Combo citas médicas';
         $assistance->save();
 
 
         $assistance = new Assistance;
         $assistance->name = 'Asistencia odontológica';
+        $assistance->description = 'Asistencia odontológica';
         $assistance->save();
 
 
         $assistance = new Assistance;
         $assistance->name = 'Asistencia financiera en hospitalización';
+        $assistance->description = 'Asistencia financiera en hospitalización';
         $assistance->save();
 
 
         $assistance = new Assistance;
         $assistance->name = 'Asistencia financiera en enfermedad grave';
+        $assistance->description = 'Asistencia financiera en enfermedad grave';
         $assistance->save();
 
     }
